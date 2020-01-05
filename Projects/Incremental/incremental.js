2d1bd1c9befa63059552a84c6851b21003642fbf
Michael Sico <michaelsico94@gmail.com>
2020-01-04 22:39:27 -0800

    From Bootstrap Template

---
+++ Projects/Incremental/incremental.js
@@ -0,0 +1,9 @@
+function showHide(ID) {
+    var elem = document.getElementById(ID);
+    if (elem.style.display != "none") {
+        elem.style.display = "none";
+    }
+    else {
+        elem.style.display = "";
+    }
+}
